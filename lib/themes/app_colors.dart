dcf685c9a53509ea6a08524e4e29900541228dd8
Михаил Кива <mihailkiva@Mihails-MacBook-Air.local>
2024-03-20 20:41:43 +0300

    Text Styles fixed

--- lib/themes/app_colors.dart
+++ lib/themes/app_colors.dart
@@ -6,9 +6,11 @@ class AppLightColors extends AppColors {
       : super(
           backgroundPrimary: const Color(0xFFFFFFFF),
           backgroundSecondary: const Color(0xFFFAF9FF),
+          disabled: const Color(0xFFE4E4E4),
           accent: const Color(0xFF3A60F3),
           accentSecondary: const Color(0xFF5779F7),
           textPrimary: const Color(0xFF4B425F),
+          textSecondary: const Color(0xFFDEDADB),
           textSurface: const Color(0xFFFFFFFF),
           error: const Color(0xFFFF4B4C),
           success: const Color(0xFF02C46D),
@@ -19,9 +21,11 @@ class AppLightColors extends AppColors {
 class AppColors extends ThemeExtension<AppColors> {
   final Color backgroundPrimary;
   final Color backgroundSecondary;
+  final Color disabled;
   final Color accent;
   final Color accentSecondary;
   final Color textPrimary;
+  final Color textSecondary;
   final Color textSurface;
   final Color error;
   final Color success;
@@ -29,9 +33,11 @@ class AppColors extends ThemeExtension<AppColors> {
   const AppColors({
     required this.backgroundPrimary,
     required this.backgroundSecondary,
+    required this.disabled,
     required this.accent,
     required this.accentSecondary,
     required this.textPrimary,
+    required this.textSecondary,
     required this.textSurface,
     required this.error,
     required this.success,
@@ -41,9 +47,11 @@ class AppColors extends ThemeExtension<AppColors> {
   ThemeExtension<AppColors> copyWith({
     Color? backgroundPrimary,
     Color? backgroundSecondary,
+    Color? disabled,
     Color? accent,
     Color? accentSecondary,
     Color? textPrimary,
+    Color? textSecondary,
     Color? textSurface,
     Color? error,
     Color? success,
@@ -51,9 +59,11 @@ class AppColors extends ThemeExtension<AppColors> {
     return AppColors(
       backgroundPrimary: backgroundPrimary ?? this.backgroundPrimary,
       backgroundSecondary: backgroundSecondary ?? this.backgroundSecondary,
+      disabled: disabled ?? this.disabled,
       accent: accent ?? this.accent,
       accentSecondary: accentSecondary ?? this.accentSecondary,
       textPrimary: textPrimary ?? this.textPrimary,
+      textSecondary: textSecondary ?? this.textSecondary,
       textSurface: textSurface ?? this.textSurface,
       error: error ?? this.error,
       success: success ?? this.success,
@@ -76,10 +86,12 @@ class AppColors extends ThemeExtension<AppColors> {
           Color.lerp(backgroundPrimary, typedOther.backgroundPrimary, t)!,
       backgroundSecondary:
           Color.lerp(backgroundSecondary, typedOther.backgroundSecondary, t)!,
+      disabled: Color.lerp(disabled, typedOther.disabled, t)!,
       accent: Color.lerp(accent, typedOther.accent, t)!,
       accentSecondary:
           Color.lerp(accentSecondary, typedOther.accentSecondary, t)!,
       textPrimary: Color.lerp(textPrimary, typedOther.textPrimary, t)!,
+      textSecondary: Color.lerp(textSecondary, typedOther.textSecondary, t)!,
       textSurface: Color.lerp(textSurface, typedOther.textSurface, t)!,
       error: Color.lerp(error, typedOther.error, t)!,
       success: Color.lerp(success, typedOther.success, t)!,
