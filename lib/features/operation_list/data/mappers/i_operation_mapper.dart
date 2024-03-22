924226932d098813e171968332a1a3287c32341d
Михаил Кива <mihailkiva@Mihails-MacBook-Air.local>
2024-03-22 19:25:40 +0300

    Merge conflicts fixed + Minor refactor in operations feature

--- lib/features/operation_list/data/mappers/i_operation_mapper.dart
+++
@@ -1,8 +0,0 @@
-import 'package:budget_tracker/features/operation_list/data/dto/operation_dto.dart';
-import 'package:budget_tracker/features/operation_list/domain/entities/operation.dart';
-
-abstract interface class OperationMapper {
-  Operation fromDto(OperationDTO dto);
-
-  OperationDTO toDto(Operation entity);
-}
--- lib/features/operation_list/internal/mapper_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:budget_tracker/features/operation_list/data/mappers/i_operation_mapper.dart';
-import 'package:budget_tracker/features/operation_list/data/mappers_impl/operation_mapper_impl.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final mapperProvider = Provider<OperationMapper>(
-  (ref) => OperationMapperImpl(),
-);
